308fedf88d08c46fded1382a040ef5a2f474620b
Andrew Reece <reece@g.harvard.edu>
2016-02-27 20:59:59 -0500

    Ratings survey working now.  Remember to vacuum.

--- run.py
+++ run.py
@@ -42,51 +42,50 @@ table_data = util.get_table_data()
 
 
 def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-	                automatic_options=True):
+				max_age=21600, attach_to_all=True, automatic_options=True):
 	''' Super-helpful decorator to avoid CORS issues when calling from Qualtrics (or anywhere else)
 		Source: https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html 
-		Also: http://stackoverflow.com/questions/22181384/javascript-no-access-control-allow-origin-header-is-present-on-the-requested'''
-    
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
+		Also: http://stackoverflow.com/questions/22181384/javascript-no-access-control-allow-origin-header-is-present-on-the-requested '''
+	
+	if methods is not None:
+		methods = ', '.join(sorted(x.upper() for x in methods))
+	if headers is not None and not isinstance(headers, basestring):
+		headers = ', '.join(x.upper() for x in headers)
+	if not isinstance(origin, basestring):
+		origin = ', '.join(origin)
+	if isinstance(max_age, timedelta):
+		max_age = max_age.total_seconds()
 
-    def get_methods():
-        if methods is not None:
-            return methods
+	def get_methods():
+		if methods is not None:
+			return methods
 
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
+		options_resp = current_app.make_default_options_response()
+		return options_resp.headers['allow']
 
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
+	def decorator(f):
+		def wrapped_function(*args, **kwargs):
+			if automatic_options and request.method == 'OPTIONS':
+				resp = current_app.make_default_options_response()
+			else:
+				resp = make_response(f(*args, **kwargs))
+			if not attach_to_all and request.method != 'OPTIONS':
+				return resp
 
-            h = resp.headers
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            h['Access-Control-Allow-Credentials'] = 'true'
-            h['Access-Control-Allow-Headers'] = \
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
+			h = resp.headers
+			h['Access-Control-Allow-Origin'] = origin
+			h['Access-Control-Allow-Methods'] = get_methods()
+			h['Access-Control-Max-Age'] = str(max_age)
+			h['Access-Control-Allow-Credentials'] = 'true'
+			h['Access-Control-Allow-Headers'] = \
+				"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+			if headers is not None:
+				h['Access-Control-Allow-Headers'] = headers
+			return resp
 
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
+		f.provide_automatic_options = False
+		return update_wrapper(wrapped_function, f)
+	return decorator
 
 
 def register_user(medium, userid, username, uid, post_ct, conn, token, secret):
@@ -163,15 +162,16 @@ def verify2(medium, uname):
 		return str(e)
 
 
+#Called by qualtrics survey for photo raters, gets instagram photos to be rated
+#- selects from urls which have not been rated at least N times (currently N=3)
+#- upon successful completion of rating survey, N is incremented by 1 for each photo rated
+#- photo urls are found in meta_ig '''
 @app.route("/getphoto")
+@nocache
 def get_photo():
-	''' Called by qualtrics survey for photo raters, gets instagram photos to be rated
-	   		- selects from urls which have not been rated at least N times (currently N=3)
-	   		- upon successful completion of rating survey, N is incremented by 1 for each photo rated
-	   		- photo urls are found in meta_ig '''
 	try:
 		conn = util.connect_db()
-
+		#data_directory = "/home/dharmahound/research.andrewgarrettreece.com/data/"
 		query_file = data_directory+"photo_ratings_queries.json"
 		med = "ig"
 		med_long = "instagram"
@@ -182,63 +182,50 @@ def get_photo():
 		ratings_query = 'and ratings_ct < {}'.format(ratings_quota) if med == 'ig' else ''
 		n_photos_from_date = 100 # this is arbitrary, set only for budget/time constraints
 		return_set_size = 20
-
-		''' REMEMBER: Right now we are:
-						1) Only using depression right now
-					  	2) Only going forward/backward from diag_date, not susp_date!
-					  	   At some point we should reach back past susp date for those <60+ days suspected '''
-
+		#REMEMBER: Right now we are:
+		#1) Only using depression 
+		#2) Only going forward/backward from diag_date, not susp_date!
+		#At some point we should reach back past susp date for those <60+ days suspected '''
 		f = open(query_file,'r')
 		query = json.loads(f.next())
-
 		q1 = query['validate']['d_from_diag'][med][condition].format(med=med,
 																	 medlong=med_long,
 																	 metatab=table,
 																	 cond=condition,
 																	 ratings_q=ratings_query)
 		df1 = pd.read_sql_query(q1, conn)
-
 		unames = "'" + "','".join(df1.username) + "'"
-		
-		q2 = query['validate']['cesd'].format(cutoff=cesd_cutoff, 
-											  names=unames)
+		q2 = query['validate']['cesd'].format(cutoff=cesd_cutoff, names=unames)
 		df2 = pd.read_sql_query(q2, conn)
-
 		unames = "'" + "','".join(df2.username) + "'"
-
 		q3 = query['validate']['date_range'].format(metatab=table,
 													cond=condition,
 													names=unames)
 		df3 = pd.read_sql_query(q3, conn, parse_dates=['created_date','diag_date'])
-
 		lt_ix = (
 				df3.ix[df3.from_diag < 0,:]
 				   .groupby('username')['from_diag']
 				   .apply(lambda x: x.nlargest(n_photos_from_date))
 				   .reset_index()['level_1']
 				 )
-
 		gt_ix = (
 				df3.ix[df3.from_diag >= 0,:]
 				   .groupby('username')['from_diag']
 				   .apply(lambda x: x.nsmallest(n_photos_from_date))
 				   .reset_index()['level_1']
 				 )
-
 		before = df3.ix[lt_ix,:].copy()
 		after = df3.ix[gt_ix,:].copy()
-
 		photos_to_rate = pd.concat([before,after])
-
 		raw_urls = photos_to_rate.url.sample(return_set_size).values
-
 		urls = {}
 		for i,row in enumerate(raw_urls):
 			urls["url"+str(i)] = row
-
 		return jsonify(urls)
 	except Exception,e:
-		return str(e)
+		return jsonify({"url0":"error"})
+
+	
 
 ##  create_table: creates new db table, using parameters stored in table_data df (global)
 ##  this is only called directly via URI - it's not part of the verify/collect pipeline
@@ -270,50 +257,70 @@ def create_table(conn, table):
 	except Exception, e:
 		return str(e)+query
 
-## add_rating: called from Qualtrics after a user has rated a photo; increments ratings_ct
-## note: the reason for the regex sub is because Flask parses / (as well as the encoded %2F) before we can even access
-##       incoming parameter strings.  since we're passing it a url, it barfs when it discovers slashes in the parameter
-##       so we just converted / to _____ (5 underscores) on the Qualtrics side, and we back-translate here.
-##       * 5 underscores may be overkill, but _ or even __ is not out of the question, just wanted to be safe.
-@app.route("/addrating/<rater_id>/<happy>/<sad>/<likable>/<interesting>/<one_word>/<description>/<encoded_url>/<epsilon>", methods=['POST', 'GET', 'OPTIONS'])
-@crossdomain(origin='*')
-def add_rating(rater_id,happy,sad,likable,interesting,one_word,description,encoded_url,epsilon,table="photo_ratings"):
 
-	# epsilon just avoids CORS issues from qualtrics
+@app.route("/addrating/<rater_id>/<happy>/<sad>/<likable>/<interesting>/<one_word>/<description>/<encoded_url>", methods=['POST', 'GET', 'OPTIONS'])
+@crossdomain(origin='*')
+def add_rating(rater_id,happy,sad,likable,interesting,one_word,description,encoded_url,table="TEST_photo_ratings"):
+	''' Called from Qualtrics after a user has rated a photo
+		- Writes ratings data to photo_ratings db
+		- Increments ratings_ct in meta_ig for that URL '''
+
+	''' NOTE: 
+		The reason for the regex sub is because Flask parses / (as well as the encoded %2F) before we can even access
+		incoming parameter strings.  Since we're passing it a url, it barfs when it discovers slashes in the parameter
+		so we just converted / to _____ (5 underscores) on the Qualtrics side, and we back-translate here.
+		5 underscores may be overkill, but _ or even __ is not out of the question, just wanted to be safe. '''
 
 	tstamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
 	try:
 		url = unquote(encoded_url).replace("_____","/")
+
+		valid_ratings = False if description == "_" else True 
+
 		fields = ['rater_id','happy','sad','likable','interesting','one_word','description']
 		values = map(unquote, [rater_id, happy, sad, likable, interesting, one_word, description])
-
-
+		
 		log_dir = 'ratings/{rid}__{t}.log'.format(rid=values[0],url=url,t=tstamp)
 		log_msgs = []
 		log_msgs.append('\nStarting add_rating for photo url {} [rater id: {}]'.format(url,values[0]))
 
 		conn = util.connect_db()
 
-		with conn:
-			query1 = "UPDATE meta_ig SET ratings_ct=ratings_ct+1 WHERE url='{}'".format(url)
-			cur = conn.cursor()
-			cur.execute(query1)
+		if valid_ratings:
 
-			for i,field in enumerate(fields):
-				try:
-					val = values[i]
-					query2 = "UPDATE {table} SET {f}='{val}' WHERE url='{url}'".format(table=table, f=field, val=val, url=url)
-					cur.execute(query2)
-				except Exception,e:
-					return query2+"__"+str(e)
-		conn.commit()
+			with conn:
+				query1 = "UPDATE meta_ig SET ratings_ct=ratings_ct+1 WHERE url='{}'".format(url)
+				cur = conn.cursor()
+				cur.execute(query1)
 
-		log_msgs.append('\Rating for url: {} [rater id: {}] stored successfully!'.format(url,values[0]))
-		util.log(log_msgs,log_dir,full_path_included=True)
-		return query2
+				for i,field in enumerate(fields):
+					try:
+						val = values[i]
+						query2 = "UPDATE {table} SET {f}='{val}' WHERE url='{url}'".format(table=table, f=field, val=val, url=url)
+						cur.execute(query2)
+					except Exception,e:
+						return query2+"__"+str(e)
+			conn.commit()
+
+			log_msgs.append('\nRating for url: {} [rater id: {}] stored successfully!'.format(url,values[0]))
+			util.log(log_msgs,log_dir,full_path_included=True)
+			return query2
+
+		else:
+
+			with conn:
+				query1 = "UPDATE meta_ig SET valid_url=404 WHERE url='{}'".format(url)
+				cur = conn.cursor()
+				cur.execute(query1)
+			conn.commit()
+
+			log_msgs.append('\nFAILED TO LOAD url: {} [rater id: {}]'.format(url,values[0]))
+			util.log(log_msgs,log_dir,full_path_included=True)
+			return 'failed to load photo url in survey'
+
 	except Exception,e:
-		log_msgs.append('\Error recording rating for url: {} [rater id: {}] [ERROR: {}]'.format(url,values[0],str(e)))
+		log_msgs.append('\nError recording rating for url: {} [rater id: {}] [ERROR: {}]'.format(url,values[0],str(e)))
 		util.log(log_msgs,log_dir,full_path_included=True)
 		return str(e)
 
@@ -470,7 +477,7 @@ def invite(action):
 	except Exception,e:
 		return str(e)
 
-		
+
 app.secret_key = os.environ.get("SECRET_KEY")
 
 ## run flask
